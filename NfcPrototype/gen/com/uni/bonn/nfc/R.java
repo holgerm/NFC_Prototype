f6fc7328b5533a9b5fddd6aa5f3cf47ac522f921
shubhambans@gmail.com <shubhambans@gmail.com>
2013-09-23 19:04:39 +0200

    Version 1 completed. need to replace video files with actual game content.

--- NfcPrototype/gen/com/uni/bonn/nfc/R.java
+++ NfcPrototype/gen/com/uni/bonn/nfc/R.java
@@ -25,16 +25,12 @@ public final class R {
         public static final int splash_screen=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
-        public static final int banner=0x7f090001;
-        public static final int video_view=0x7f090000;
+        public static final int banner=0x7f080001;
+        public static final int video_view=0x7f080000;
     }
     public static final class layout {
         public static final int home_layout=0x7f030000;
         public static final int splash_screen=0x7f030001;
-    }
-    public static final class menu {
-        public static final int home=0x7f080000;
     }
     public static final class raw {
         public static final int five=0x7f040000;
@@ -47,8 +43,15 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int alert_title=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int btn_no=0x7f050007;
+        public static final int btn_ok=0x7f050005;
+        public static final int btn_yes=0x7f050006;
+        public static final int empty_matchbox=0x7f050009;
+        public static final int err_invalid_action=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int ignite_candle=0x7f050008;
     }
     public static final class style {
         /** 
@@ -60,17 +63,6 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
--- NfcPrototype/src/com/uni/bonn/nfc/HomeActivity.java
+++ NfcPrototype/src/com/uni/bonn/nfc/HomeActivity.java
@@ -1,24 +1,35 @@
 package com.uni.bonn.nfc;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.net.Uri;
-import android.nfc.NfcAdapter;
+import android.nfc.FormatException;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.widget.VideoView;
 
 import com.uni.bonn.nfc4mg.NFCEventManager;
+import com.uni.bonn.nfc4mg.constants.TagConstants;
+import com.uni.bonn.nfc4mg.exception.NfcTagException;
+import com.uni.bonn.nfc4mg.nfctag.ParseTagListener;
+import com.uni.bonn.nfc4mg.nfctag.TagHandler;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements ParseTagListener {
 
 	private static final String TAG = "HomeActivity";
 
 	private static VideoView mVideoView;
-	private String mVideoPath = "";
 
 	private NFCEventManager mNFCEventManager = null;
+
+	// Game parameters.
+	private TagHandler mTagHamdler;
+	private int mCurrentInteraction = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +44,22 @@ public class HomeActivity extends Activity {
 			e.printStackTrace();
 		}
 
+		try {
+			mTagHamdler = new TagHandler(this, this);
+		} catch (NfcTagException e) {
+			e.printStackTrace();
+		}
+
 		mVideoView = (VideoView) findViewById(R.id.video_view);
 
-		mVideoPath = "android.resource://" + getPackageName() + "/" + R.raw.one;
-		mVideoView.setVideoURI(Uri.parse(mVideoPath));
-
+		String path = "android.resource://" + getPackageName() + "/"
+				+ R.raw.one;
+		setnPlayVideo(path);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
-		return true;
+	private void setnPlayVideo(final String path) {
+		mVideoView.setVideoURI(Uri.parse(path));
+		mVideoView.start();
 	}
 
 	@Override
@@ -55,9 +70,10 @@ public class HomeActivity extends Activity {
 			mNFCEventManager.removeNFCListener(HomeActivity.this);
 		}
 
-		if (null != mVideoView && mVideoView.isPlaying()) {
-			mVideoView.pause();
-		}
+		/*
+		 * if (null != mVideoView && mVideoView.isPlaying()) {
+		 * mVideoView.pause(); }
+		 */
 	}
 
 	@Override
@@ -68,17 +84,194 @@ public class HomeActivity extends Activity {
 			mNFCEventManager.attachNFCListener(HomeActivity.this);
 		}
 
-		if (null != mVideoView) {
-			mVideoView.start();
-		}
+		/*
+		 * if (null != mVideoView) { mVideoView.start(); }
+		 */
 	}
 
 	@Override
 	protected void onNewIntent(Intent intent) {
 
-		if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
-
-			Log.v(TAG, "Intent Action :: ACTION_TAG_DISCOVERED");
+		try {
+			mTagHamdler.processIntent(intent);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NfcTagException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+
+	@Override
+	public void onStartParsing(String msg) {
+
+	}
+
+	@Override
+	public void onParseComplete(int tagType) {
+
+		switch (tagType) {
+
+		case TagConstants.TAG_TYPE_INFO:
+
+			String data = mTagHamdler.getmInfoTagModel().getData();
+			handleInteraction(data);
+			break;
+		case TagConstants.TAG_TYPE_RESOURCE:
+
+			String resName = mTagHamdler.getmResourceTagModel().getName();
+			int resCount = mTagHamdler.getmResourceTagModel().getCount();
+			handleResourceInteraction(resName, resCount);
+
+			break;
+		}
+	}
+
+	
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+
+		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+		switch(id){
+		
+		case 1:
+			alertBuilder.setTitle(R.string.alert_title);
+			alertBuilder.setMessage(R.string.err_invalid_action);
+			alertBuilder.setCancelable(true);
+			alertBuilder.setPositiveButton(R.string.btn_ok, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+
+					dialog.dismiss();
+
+				}
+			});
+			break;
+		case 2:
+
+			alertBuilder.setMessage(R.string.ignite_candle);
+			alertBuilder.setCancelable(true);
+			alertBuilder.setPositiveButton(R.string.btn_yes, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+
+					dialog.dismiss();
+					// Set the interaction counter to next scene
+					mCurrentInteraction = 4;
+					String path = "android.resource://" + getPackageName() + "/"
+							+ R.raw.five;
+					setnPlayVideo(path);
+
+				}
+			}).setNegativeButton(R.string.btn_no, new OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					
+					dialog.dismiss();
+				}
+			});
+
+			
+			break;
+		}
+		
+		
+		// create alert dialog
+		AlertDialog alertDialog = alertBuilder.create();
+
+		return alertDialog;
+	}
+
+	private void handleResourceInteraction(final String data, final int resCount) {
+
+		if (mCurrentInteraction == 2 && "matchbox".equalsIgnoreCase(data)) {
+
+			mCurrentInteraction = 3;
+			if (0 != resCount) {
+				String path = "android.resource://" + getPackageName() + "/"
+						+ R.raw.four;
+				setnPlayVideo(path);
+			}
+		}else if (mCurrentInteraction == 4 && "matchbox".equalsIgnoreCase(data)) {
+
+			mCurrentInteraction = 5;
+			if (0 != resCount) {
+				String path = "android.resource://" + getPackageName() + "/"
+						+ R.raw.six;
+				setnPlayVideo(path);
+			}
+		}
+
+	}
+
+	private void handleInteraction(final String data) {
+
+		if (mCurrentInteraction == 0 && "concert_ticket".equalsIgnoreCase(data)) {
+
+			// Set the interaction counter to next scene
+			mCurrentInteraction = 1;
+			String path = "android.resource://" + getPackageName() + "/"
+					+ R.raw.two;
+			setnPlayVideo(path);
+
+		} else if (mCurrentInteraction == 1
+				&& "milchschnitte".equalsIgnoreCase(data)) {
+
+			// Set the interaction counter to next scene
+			mCurrentInteraction = 2;
+			String path = "android.resource://" + getPackageName() + "/"
+					+ R.raw.three;
+			setnPlayVideo(path);
+			
+		} else if (mCurrentInteraction == 3
+				&& "candle".equalsIgnoreCase(data)) {
+
+			showDialog(2);
+			
+		}else if (mCurrentInteraction == 5
+				&& "key".equalsIgnoreCase(data)) {
+			
+			mCurrentInteraction = 6;
+			String path = "android.resource://" + getPackageName() + "/"
+					+ R.raw.seven;
+			setnPlayVideo(path);
+			
+		}else if (mCurrentInteraction == 6
+				&& "maps".equalsIgnoreCase(data)) {
+			
+			mCurrentInteraction = 7;
+			String path = "android.resource://" + getPackageName() + "/"
+					+ R.raw.one;
+			setnPlayVideo(path);
+			
+		}else if (mCurrentInteraction == 7
+				&& "bike_key".equalsIgnoreCase(data)) {
+			
+			mCurrentInteraction = 8;
+			String path = "android.resource://" + getPackageName() + "/"
+					+ R.raw.two;
+			setnPlayVideo(path);
+			
+		}else if (mCurrentInteraction == 8
+				&& "number_lock".equalsIgnoreCase(data)) {
+			
+			mCurrentInteraction = 9;
+			String path = "android.resource://" + getPackageName() + "/"
+					+ R.raw.three;
+			setnPlayVideo(path);
+			
+		} else {
+
+			showDialog(1);
+		}
+	}
+
 }
